bc9635c597037209aa90b8c7a254f6637955957e
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-02 19:09:23 +0500

    Add react-router-dom and site responsive header and Uzbekistan page. Create RootLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import RootLayout from './layouts/RootLayout';
+import Uzbekistan from './pages/Uzbekistan';
 
 const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<RootLayout />}>
+        <Route path='/' element={<Uzbekistan />}></Route>
+      </Route>
+    )
+  );
   return (
     <>
+      <RouterProvider router={router} />
     </>
   );
 };
---
+++ src/pages/Uzbekistan.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Uzbekistan = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default Uzbekistan;
