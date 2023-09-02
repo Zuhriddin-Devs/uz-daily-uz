0689cc3a8f278df22292f10bcb744208b3be1de3
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-02 19:22:43 +0500

    Add main and import Outlet

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
 
 const RootLayout = () => {
   return (
     <>
       <Header />
+      <main className='w-full bg-gray-200 py-16 mt-16'>
+        <Outlet />
+      </main>
     </>
   );
 };
