b921f15201d58fc3d5f67a26b2abe1ca57622d6d
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-03 15:41:15 +0500

    Add Iqtisod.jsx and Route

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 } from "react-router-dom";
 import RootLayout from './layouts/RootLayout';
 import Uzbekistan from './pages/Uzbekistan';
+import Iqtisod from './pages/Iqtisod';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<RootLayout />}>
         <Route path='/' element={<Uzbekistan />}></Route>
+        <Route path='/Iqtisod' element={<Iqtisod />}></Route>
       </Route>
     )
   );
