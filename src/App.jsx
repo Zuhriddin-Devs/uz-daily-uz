31b5b73347d2f682c498716abcda1e89569deaba
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-03 16:40:10 +0500

    Add Texnologiyalar, Madaniyat, sport, Turizm pages and Route. Add cards to pages

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,23 @@ import {
 import RootLayout from './layouts/RootLayout';
 import Uzbekistan from './pages/Uzbekistan';
 import Iqtisod from './pages/Iqtisod';
+import Moliya from './pages/Moliya';
+import Texnologiyalar from './pages/Texnologiyalar';
+import Madaniyat from './pages/Madaniyat';
+import Sport from './pages/Sport';
+import Turizm from './pages/Turizm';
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route element={<RootLayout />}>
-        <Route path='/' element={<Uzbekistan />}></Route>
-        <Route path='/Iqtisod' element={<Iqtisod />}></Route>
+        <Route path='/' element={<Uzbekistan />} />
+        <Route path='/Iqtisod' element={<Iqtisod />} />
+        <Route path='/Moliya' element={<Moliya />} />
+        <Route path='/Texnologiyalar' element={<Texnologiyalar />} />
+        <Route path='/Madaniyat' element={<Madaniyat />} />
+        <Route path='/Sport' element={<Sport />} />
+        <Route path='/Turizm' element={<Turizm />} />
       </Route>
     )
   );
---
+++ src/pages/Turizm.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import data from '../data';
+import { Link } from 'react-router-dom';
+
+const Turizm = () => {
+    return (
+        <div className="w-full max-w-7xl mx-auto grid sm:grid-cols-2 gap-x-10 gap-y-16 px-5">
+            <div>
+                <div className='mb-10'>
+                    <h1 className='text-black text-3xl font-medium text-center border-b-2 border-red-500 pb-5'>
+                        Turizm
+                    </h1>
+                </div>
+                <div>
+                    <ul className='grid grid-cols-1 gap-y-7'>
+                        {data().slice(60, 70).map((news) => (
+                            <li key={news.id} className='bg-white grid lg:grid-cols-2 gap-6 shadow-lg rounded-md p-5'>
+                                <div>
+                                    <img className='w-full h-full rounded-md' src={news.image} alt={news.alt} />
+                                </div>
+                                <div className='space-y-5'>
+                                    <h2 className="text-black text-lg font-medium">{news.title}</h2>
+                                    <p className="text-gray-600 text-sm font-normal">{news.about}</p>
+                                    <div className='flex justify-between items-center'>
+                                        <p className="text-black text-base font-medium">{news.date}</p>
+                                        <Link to="#" className="text-black text-base font-medium">{news.detail}</Link>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <div className='mb-10'>
+                    <h1 className='text-black text-3xl font-medium text-center border-b-2 border-red-500 pb-5'>
+                        So'nggi yangiliklar
+                    </h1>
+                </div>
+                <div>
+                    <ul className='grid grid-cols-1 gap-y-7'>
+                        {data().slice(0, 3).map((news) => (
+                            <li key={news.id} className='bg-white grid lg:grid-cols-2 gap-6 shadow-lg rounded-md p-5'>
+                                <div>
+                                    <img className='w-full h-full rounded-md' src={news.image} alt={news.alt} />
+                                </div>
+                                <div className='space-y-5'>
+                                    <h2 className="text-black text-lg font-medium">{news.title}</h2>
+                                    <p className="text-gray-600 text-sm font-normal">{news.about}</p>
+                                    <div className='flex justify-between items-center'>
+                                        <p className="text-black text-base font-medium">{news.date}</p>
+                                        <Link to="#" className="text-black text-base font-medium">{news.detail}</Link>
+                                    </div>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Turizm;
