dbf965763226681ac6870404ca44268ee530b4b2
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-16 17:40:30 +0500

    A link to social networks has been add

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,24 +1,22 @@
 import { FaFacebookF } from "react-icons/fa";
 import { AiOutlineTwitter } from "react-icons/ai";
-import { AiFillSkype } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 import { BiLogoTelegram } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import logo from '../assets/logo.png';
 
 function Footer() {
     const iconsTab = [
-        { icon: <FaFacebookF /> },
-        { icon: <AiOutlineTwitter /> },
-        { icon: <AiFillSkype /> },
-        { icon: <BiLogoTelegram /> },
+        { icon: <FaFacebookF />, link: 'https://www.facebook.com/UzDaily.uz/?locale=ru_RU' },
+        { icon: <AiOutlineTwitter />, link: 'https://twitter.com/UzDailycom' },
+        { icon: <AiFillLinkedin />, link: 'https://ru.linkedin.com/company/daily-media' },
+        { icon: <BiLogoTelegram />, link: 'https://t.me/UZDAILY' },
     ];
 
     return (
         <footer className="w-full bg-gray-800 py-20">
             <div className="w-full max-w-7xl mx-auto px-5">
-                {/* footer div all */}
                 <div className="flex justify-between flex-col md:flex-row items-center md:items-start md:gap-[5rem] text-left">
-                    {/* logo side */}
                     <div className="flex flex-col w-1/2 md:p-0 py-5 gap-8 mb-5 md:mb-0">
                         <Link to='/'>
                             <img
@@ -30,12 +28,12 @@ function Footer() {
                         <p className="text-base font-medium text-white">
                             Hamma huquqlar himoyalangan. UzDaily.uz Internet nashri OAB sifatida 0601 raqami bilan 28.10.2009 - yil davlat ro'yxatidan o'tgan. Hamma xizmatlar litsenziyalangan, mahsulotlar esa sertifikatlarga ega.
                         </p>
-                        {/* socials */}
                         <div className="flex gap-7 text-lg text-gray-800 justify-center md:justify-start">
-                            {iconsTab.map(({ icon }, index) => {
+                            {iconsTab.map(({ icon, link }, index) => {
                                 return (
                                     <Link
-                                        to='#'
+                                        to={link}
+                                        target="_blank"
                                         key={index}
                                         className="text-2xl bg-white p-2 rounded-full hover:bg-blue-500 hover:text-white cursor-pointer"
                                         style={{ transition: "all 0.3s" }}
@@ -50,7 +48,6 @@ function Footer() {
                         </p>
                     </div>
 
-                    {/* middle div */}
                     <div className="flex flex-col gap-8 relative">
                         <p className="text-2xl text-white font-bold border-b-2 border-red-500 pb-2">Biz haqimizda</p>
 
@@ -71,7 +68,6 @@ function Footer() {
                         </Link>
                     </div>
 
-                    {/* middle div */}
                     <span></span>
                 </div>
             </div>
