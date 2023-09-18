d47e94a054a944ffe84c82707c39769b164b9ab1
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-09-18 07:42:01 +0500

    Change the main responsive

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -7,7 +7,7 @@ const RootLayout = () => {
   return (
     <>
       <Header />
-      <main className='w-full bg-gray-200 py-12 mt-16'>
+      <main className='w-full bg-gray-200 py-12 mt-32 lg:mt-16'>
         <Outlet />
       </main>
       <Footer />
