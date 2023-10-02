9f94277085ca7adbc6a1de48f3fc2441d5926889
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-10-02 18:56:13 +0500

    class changed to className

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,15 +3,15 @@ import React from 'react';
 const Loader = () => {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <div class="hourglassBackground">
-        <div class="hourglassContainer">
-          <div class="hourglassCurves"></div>
-          <div class="hourglassCapTop"></div>
-          <div class="hourglassGlassTop"></div>
-          <div class="hourglassSand"></div>
-          <div class="hourglassSandStream"></div>
-          <div class="hourglassCapBottom"></div>
-          <div class="hourglassGlass"></div>
+      <div className="hourglassBackground">
+        <div className="hourglassContainer">
+          <div className="hourglassCurves"></div>
+          <div className="hourglassCapTop"></div>
+          <div className="hourglassGlassTop"></div>
+          <div className="hourglassSand"></div>
+          <div className="hourglassSandStream"></div>
+          <div className="hourglassCapBottom"></div>
+          <div className="hourglassGlass"></div>
         </div>
       </div>
     </div>
